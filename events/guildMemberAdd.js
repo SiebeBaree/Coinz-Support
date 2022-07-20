ff0ba3e459dfdb613900ae5ce12fd95dfbb36b7d
Siebe Baree <siebe.baree@outlook.com>
2022-07-20 21:28:55 +0200

    Added suspicious accounts check

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 
-const minimumDays = 3; // use 0 to disable
+const minimumDays = 5; // use 0 to disable
 
 const giveRole = async (member, roldId) => {
     const role = member.guild.roles.cache.get(roldId);
@@ -9,10 +9,8 @@ const giveRole = async (member, roldId) => {
 
 module.exports = async (client, member) => {
     // Check for suspicious accounts
-    if (!member.user.flags.any(...[1 << 0, 1 << 1, 1 << 2, 1 << 3, 1 << 6, 1 << 7, 1 << 8, 1 << 9, 1 << 14, 1 << 17, 1 << 18])) {
-        if (member.user.avatarURL() === null) await giveRole(member, '939840999977525248');
-        if (member.user.createdTimestamp <= Date.now() - (86400 * minimumDays)) await giveRole(member, '939840999977525248')
-    }
+    if (member.user.avatarURL() === null) await giveRole(member, '939840999977525248');
+    if (member.user.createdTimestamp <= Date.now() - (86400 * minimumDays)) await giveRole(member, '939840999977525248')
 
     // Send welcome message
     const embed = new MessageEmbed()
