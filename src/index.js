55f670ec4e6fd2f4860451b8d1ed0522691d673a
Siebe Baree <siebe.baree@outlook.com>
2022-12-12 14:13:23 +0100

    Updated to new infrastructure

--- src/index.js
+++ src/index.js
@@ -1,60 +1,49 @@
-require('dotenv').config();
-const fs = require('fs');
-const mongoose = require('mongoose');
-const util = require('util');
-const readdir = util.promisify(fs.readdir);
-const { Client, Collection } = require('discord.js');
+import dotenv from "dotenv";
+dotenv.config();
 
-// Initialising a Client object
-const client = new Client({ partials: ["CHANNEL"], intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
+import Bot from "./structures/Bot.js"
+import { ActivityType, GatewayIntentBits, Partials } from "discord.js"
+import mongoose from "mongoose"
+const { connect } = mongoose;
 
-// Importing all tools and creating collections
-client.commands = new Collection();
-client.event = new Collection();
-client.config = require('./config.json');
-client.logger = require('./tools/logger');
-client.tools = require('./tools/tools');
-
-async function init() {
-    // Event handler
-    const eventFiles = fs.readdirSync('./events/').filter(file => file.endsWith('.js'));
-    for (const file of eventFiles) {
-        const event = require(`${process.cwd()}/events/${file}`);
-        const eventName = file.split(".")[0];
-        client.logger.load(`Loading event ${file}.`);
-        client.on(eventName, event.bind(null, client));
+const bot = global.bot = new Bot({
+    partials: [Partials.GuildMember],
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessageReactions],
+    presence: {
+        activities: [{
+            name: "coinzbot.xyz",
+            type: ActivityType.Playing
+        }], status: "online"
     }
+});
 
-    // Command Handler
-    let commandFolder = await readdir("./commands/");
-    commandFolder.forEach(dir => {
-        const commandFiles = fs.readdirSync('./commands/' + dir + "/").filter(file => file.endsWith('.js'));
-        for (const file of commandFiles) {
-            const command = require(`${process.cwd()}/commands/${dir}/${file}`);
+// Connect to MongoDB Database
+connect(process.env.DATABASE_URI, {
+    dbName: "coinz_beta",
+    useNewUrlParser: true,
+    maxPoolSize: 100,
+    minPoolSize: 5,
+    family: 4,
+    heartbeatFrequencyMS: 30000,
+    keepAlive: true,
+    keepAliveInitialDelay: 300000
+}).then(() => bot.logger.ready('Connected to MongoDB'));
 
-            try {
-                client.commands.set(command.help.name, command);
-            } catch (err) {
-                client.logger.error(`An unexpected error occured with COMMAND ${file}.`);
-            }
-        }
-    })
+bot.login(process.env.DISCORD_TOKEN);
+bot.rest.on('rateLimit', rateLimitData => {
+    bot.logger.warn(`RATELIMITED for ${Math.floor(rateLimitData.timeout / 1000)} seconds | Limit: ${rateLimitData.limit} requests | Global? ${rateLimitData.global ? "yes" : "no"}`);
+});
 
-    // Connect to the database
-    mongoose.connect(process.env.DATABASE_URI, { dbName: "coinz_support" }).then(() => {
-        client.logger.ready(`Connected to MongoDB.`);
-    }).catch((err) => {
-        client.logger.error('Unable to connect to MongoDB Database.\nError: ' + err);
-    })
+// Global Error Handler
+const ignoredErrors = ["DiscordAPIError[10008]: Unknown Message"];
+process.on('uncaughtException', (err) => {
+    if (!ignoredErrors.includes(`${err.name}: ${err.message}`)) {
+        bot.logger.error(err.stack);
+    }
+});
 
-    await client.login(process.env.TOKEN);
-}
-
-init();
-
-process.on('unhandledRejection', err => {
-    client.logger.error(`Unknown error occured:\n`);
-    console.log(err)
-})
-
-module.exports.client = client;
+process.on('unhandledRejection', (err) => {
+    if (!ignoredErrors.includes(`${err.name}: ${err.message}`)) {
+        bot.logger.error(err.stack);
+    }
+});
