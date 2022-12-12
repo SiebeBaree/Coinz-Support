5ee6cd9b1295e9393aaef646f5c8d56ef6645d1e
Siebe Baree <siebe.baree@outlook.com>
2022-12-12 14:12:28 +0100

    Removed old command

--- src/schemas/modmail.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = mongoose.model("modmail", new mongoose.Schema({
-    userId: { type: String, require: true },
-    channelId: { type: String, require: true }
-}));
